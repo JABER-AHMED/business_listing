2156b270396435f4af3d1fe4b44c9fca2bcb681f
JABER-AHMED <placidcse@gmail.com>
2017-07-26 20:11:56 +0600

    create ListingsController and view

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
+
+Route::get('/index', 'ListingsController@index')->name('index');
